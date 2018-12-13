cdc9a85528171a8b41774bd25ff0f2b0725e11d6
twiceYuan <twiceyuan@gmail.com>
2018-12-13 11:56:05 +0800

    升级 Kotlin 版本

--- permissionx-kotlin/src/main/java/com/twiceyuan/permissionx/kotlin/permissionsx.kt
+++ permissionx-kotlin/src/main/java/com/twiceyuan/permissionx/kotlin/permissionsx.kt
@@ -1,13 +1,20 @@
+@file:Suppress("unused")
+
 package com.twiceyuan.permissionx.kotlin
 
 import android.app.Activity
 import android.support.v4.app.Fragment
+import com.twiceyuan.permissionx.PermissionRequestHolder
 import com.twiceyuan.permissionx.PermissionX
 
-fun Activity.requestPermissionX(vararg permissions: String) = PermissionX.request(this, permissions)
+fun Activity.requestPermissionX(vararg permissions: String): PermissionRequestHolder =
+        PermissionX.request(this, permissions)
 
-fun Fragment.requestPermissionX(vararg permissions: String) = PermissionX.request(this, permissions)
+fun Fragment.requestPermissionX(vararg permissions: String): PermissionRequestHolder =
+        PermissionX.request(this, permissions)
 
-fun Activity.requestPermissionX(permissions: Collection<String>) = PermissionX.request(this, permissions)
+fun Activity.requestPermissionX(permissions: Collection<String>): PermissionRequestHolder =
+        PermissionX.request(this, permissions)
 
-fun Fragment.requestPermissionX(permissions: Collection<String>) = PermissionX.request(this, permissions)
+fun Fragment.requestPermissionX(permissions: Collection<String>): PermissionRequestHolder =
+        PermissionX.request(this, permissions)
